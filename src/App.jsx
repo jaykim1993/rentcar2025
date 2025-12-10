30d7de038c91b9536a5d15c7cc3a6b636431e4e7
r-eong <ryungkyung28@gmail.com>
2025-12-10 17:13:22 +0900

    잠시저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
 
+import Home from './pages/Home'
+// import RentalCalendar from './contexts/calendercontext'
+import Footer from './common/Footer'
+
 function App() {
 
   return (
     <>
-      
+    <Home />
+    {/* <RentalCalendar /> */}
+    <Footer />
     </>
   )
 }
