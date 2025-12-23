62c6e2d5f40883c90c9e20ec866cad2f531cbfbb
parkch9911 <son20032002@gmail.com>
2025-12-23 10:31:43 +0900

    1223

--- src/pages/MypageBooked.jsx
+++ src/pages/MypageBooked.jsx
@@ -2,30 +2,29 @@ import { useContext } from "react";
 import { BookingContext } from "../contexts/Bookingcontext";
 import { AuthContext } from "../contexts/Authcontext";
 import { DataContext } from "../contexts/Datacontext";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./MypageBooked.css";
 
 export default function MypageBooked() {
-  const { myBookings } = useContext(BookingContext); // 예약내역 보기 함수 호출
+  const { myBookings, bookedlistAll } = useContext(BookingContext); // 예약내역 보기 함수 호출
   const { userid, username } = useContext(AuthContext); // 유저 정보 호출
-
-  // 예약차량 예외처리
-  if (!userid) return <p>로그인 후 이용해주세요.</p>;
-  if (!myBookings.length) return <p>최근 본 차량이 없습니다.</p>;
-
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    const [y, m, d] = myBookings[0].startDate.split('-');
-    const startDate = new Date(y, m - 1, d);
-    const diffDays = Math.ceil(
-      (startDate - today) / (1000 * 60 * 60 * 24)
-    );
   
+    console.log(myBookings)
+    console.log(bookedlistAll)
+
   return (
-    <>
+    <> 
+    <h1 className="guideMainText">마이페이지</h1>
+    <h2 className="guideMainText">예약내역</h2>
    <div className="myPageBookWarp">
+    {myBookings.length === 0?
+      <div className="mypageBookCard">
+        <i class="bi bi-exclamation-lg"></i>
+        <p>아직 예약 내역이 없습니다.</p>
+        <Link to={'/searchcarlist'}>예약하러가기</Link>
+      </div>
+      :
+    <div>
       {myBookings.map(book => {
         // 오늘 (날짜 기준)
         const today = new Date();
@@ -42,33 +41,32 @@ export default function MypageBooked() {
 
         return (
           <div key={book.id} className="mypageBookCard">
-            <img
-              src={`/images/cars/${book.car?.car_img}`}
-              alt={book.car?.model}
-            />
-
-            <div className="mypageBookinfo">
-              <h3>{book.car?.brand} {book.car?.model}</h3>
-              <h3 className="mapCarDays">{dText}</h3>
-              <p>예약번호 {book.id}</p>
-              <p>대여 지점: {book.car?.location}점</p>
-              <p>
-                {book.startDate} {book.startTime}
-                {" ~ "}
-                {book.endDate} {book.endTime}
-              </p>
-
-              <p className="price">
-                결제금액 : {book.price.toLocaleString()}원
-              </p>
-
-              <p className="booked-date">
-                예약일: {book.bookedDate}
-              </p>
+            <div>
+              <div className="mypageBookinfo">
+                <img
+                src={`/images/cars/${book.car?.car_img}`}
+                alt={book.car?.model}/>
+                <div className="tomargin">
+                  <h3 className="mapCarname">{book.car?.brand} {book.car?.model}</h3>
+                  <h3 className="mapCarDays">{dText}</h3>
+                  <p className="mapCarBooknum">예약번호 {book.id}</p>
+                  <p className="mapCarBooknum" >{book.car?.location}점</p>
+                  <p className="mapCarDate">
+                    {book.startDate} {book.startTime}
+                    {" ~ "}
+                    {book.endDate} {book.endTime}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="GoToDetailBoxmap">
+                <Link to={`/mypage/detail/${book.id}`} className="mapGoToDetail">예약 상세보기</Link>
             </div>
           </div>
         );
       })}
+      </div>   
+      }
     </div>
     </>
   );
--- src/pages/MypageDetail.jsx
+++ src/pages/MypageDetail.jsx
@@ -1,10 +1,91 @@
 import './MypageDetail.css'
+import { useContext, useState } from "react";
+import { BookingContext } from "../contexts/Bookingcontext";
+import { AuthContext } from "../contexts/Authcontext";
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function MypageDetail(){
+    const { id } = useParams(); // 예약번호
+    const { myBookings, setBookedlistAll,bookedlistAll } = useContext(BookingContext); // 예약내역 보기 함수 호출
+    const { userid, username } = useContext(AuthContext); // 유저 정보 호출
+    const bookedThis = myBookings.find(book => book.id === id);
+
+    const [showMap,setShowMap]=useState(false);
+
+    const navigate = useNavigate()
+
+    //리스트삭제핸들러
+        const bookdeleteHandle = () => {
+        setBookedlistAll(prev =>
+            prev.filter(book => book.id !== id)
+        );
+        alert("예약이 취소되었습니다.");
+        navigate("/mypage/booked");
+        };
+
+  
+
+    let date = (new Date(`${bookedThis.endDate}T${bookedThis.endTime}`)-new Date(`${bookedThis.startDate}T${bookedThis.startTime}`))/ (1000 * 60 * 30);
+    const hours = Math.floor(date / 2);
+    const minutes = (date % 2) * 30;
     return(
         <>
+            <h1 className="guideMainText">마이페이지</h1>
+            <h2 className="guideMainText">예약상세</h2>
+                
+                <div>
+                    <div>
+                        <p>{username}</p>
+                        <p>예약번호 : {bookedThis.id}</p>
+                        <p>출발일시 : {bookedThis.startDate} - {bookedThis.startTime}</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>{bookedThis.car.brand}</p>
+                            <p>{bookedThis.car.model}</p>
+                        </div>
+                        <div>
+                            <img src={`/images/cars/${bookedThis.car?.car_img}`}/>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>
+                                <p>지역</p>
+                                <h4>{bookedThis.car.location}점</h4>
+                            </div>
+                            <div>
+                                <button>지도보기</button>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p>일정</p>
+                                <h4>{bookedThis.startDate}-{bookedThis.startTime} ~ 
+                                    {bookedThis.endDate}-{bookedThis.endTime} 
+                                </h4>
+                                <p>이용 시간: {hours}시간{minutes ? ` ${minutes}분` : ""}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p>차량</p>
+                                <h4>{bookedThis.car.model}</h4>
+                                <h4>{bookedThis.car.model_year}년식</h4>
+                                <h4>연료타입 : {bookedThis.car.fuel_type}</h4>
+                                <p>이용 시간: {hours}시간{minutes ? ` ${minutes}분` : ""}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button onClick={bookdeleteHandle}>예약 취소하기</button>
+                        </div>
+                    </div>
+                
+                </div>
+                :
 
-    
         </>
     )
 }
