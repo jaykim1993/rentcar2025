343710a96b6dc04e6f47538fef60c0b96475eddc
parkch9911 <son20032002@gmail.com>
2025-12-23 13:03:31 +0900

    1223-1303

--- src/pages/MypageInquiry.jsx
+++ src/pages/MypageInquiry.jsx
@@ -1,6 +1,7 @@
 import './MypageInquiry.css'
 import { useContext } from "react"
 import { AuthContext } from '../contexts/Authcontext';
+import { Link } from 'react-router-dom';
 
 export default function MypageInquiry(){
 
@@ -13,19 +14,58 @@ export default function MypageInquiry(){
         const myInquiries = allInquiries.filter(item => item.userid === userid);
         console.log(myInquiries)
 
+        const seeinquiry = ()=>{
+            alert('응답이 완료된 후 열람할 수 있습니다.')
+        }
     return(
         <>
         {/* 밑에는 1:1문의 div */}
      <div className="mypage-inquries">
             <h1 className="guideMainText">마이페이지</h1>
             <h2 className="guideMainText">1:1문의내역</h2>
-            {myInquiries.length === 0 && <p>문의 내역이 없습니다.</p>}
-            {myInquiries.map((data) => (
-                <div key={data.id}>
-                    <h4>{data.title}</h4>
-                    <p>{data.content}</p>
-                </div>
-            ))}
+            <div className='MyInquiryWrap'>
+                {myInquiries.length === 0 ? (
+                <div>
+                    <div className="mypageBookCard">
+                        <i class="bi bi-exclamation-lg warningIcon"></i>
+                        <p className="noBookedP">아직 문의내역이 없습니다.</p>
+                    </div>
+                    <Link to={'/customerservice'} className="noBookedGoToBook">고객센터로 이동하기</Link>
+                </div> 
+                 ) : (
+                <table className="mypage-inquiries-table">
+                    <thead>
+                        <tr>
+                        <th className='mytable1'>번호</th>
+                        <th className='mytable2'>제목</th>
+                        <th className='mytable3'>내용</th>
+                        <th className='mytable4'>작성일</th>
+                        <th className='mytable5'>상태</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {myInquiries.length === 0 ? (
+                        <tr>
+                            <td colSpan="4" style={{ textAlign: "center" }}>
+                            문의 내역이 없습니다.
+                            </td>
+                        </tr>
+                        ) : (
+                        myInquiries.map((data, index) => (
+                            <tr onClick={seeinquiry} className='myinquTr' key={data.id}>
+                                <td className='mytable1TD'>{myInquiries.length - index}</td>
+                                <td className='mytable2TD'>{data.title}</td>
+                                <td className='mytable3TD'>{data.content}</td>
+                                <td className='mytable4TD'>{data.whenCreate}</td>
+                                <td className='mytable5TD'><span>대기중</span></td>
+                            </tr>
+                        ))
+                        )}
+                    </tbody>
+                </table>
+                )
+                }
+            </div>
         </div>
         </>
     )
